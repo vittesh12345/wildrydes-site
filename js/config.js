2adaf01bfc9c3bd8a92dbe57b8fe9d6ef69518f2
vittesh12345 <49050174+vittesh12345@users.noreply.github.com>
2025-06-23 20:36:12 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_PPkynf3wh', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '16kjssnfp8bm7v6ugksic72rl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_mOCBBb1jn',
+        userPoolClientId: '6k1mck3v6rf5mas0pordn86d5p',
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: '' // You can leave this empty for now
     }
 };
